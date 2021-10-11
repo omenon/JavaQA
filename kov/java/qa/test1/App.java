c10d94648916c4da759a306f0b23721dd01cbed6
ikov <omenon1995@gmail.com>
2021-10-11 15:10:43 +0300

    add test1.1

--- kov/java/qa/test1/App.java
+++ kov/java/qa/test1/App.java
@@ -3,5 +3,7 @@ package kov.java.qa.test1;
 public class App {
     public static void main(String[ ] args)   {
         System.out.println("My first program");
+        System.out.println("Text1"); 
+        System.out.println("Text 1 " + "\n" +"Text 2");
     }
 }
