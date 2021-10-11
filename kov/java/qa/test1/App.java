90799a29d5fb82daf0e23b7640e83d52c4160a25
ikov <omenon1995@gmail.com>
2021-10-11 15:26:01 +0300

    add test1.2

--- kov/java/qa/test1/App.java
+++ kov/java/qa/test1/App.java
@@ -1,9 +1,19 @@
 package kov.java.qa.test1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class App {
-    public static void main(String[ ] args)   {
-        System.out.println("My first program");
-        System.out.println("Text1"); 
-        System.out.println("Text 1 " + "\n" +"Text 2");
-    }
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+System.out.println("Hello. What is your name?");
+String name = br.readLine();
+System.out.println("How old are you?");
+int age = Integer.parseInt(br.readLine());
+
+System.out.println("Hello " + name);  
+System.out.println("You are " + age);
+  }
 }
